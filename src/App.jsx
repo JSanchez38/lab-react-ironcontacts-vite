3eb747f03f099dc0d093ec891079683c74d1b4d3
Jesús Sánchez <jsxexu38@gmail.com>
2024-04-04 15:00:00 +0200

    solved without iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import "./App.css";
-import contactList from "./contacts.json";
-import { useState } from 'react'
+import ContactList from "./components/ContactList";
+
 
 
 function App() {
-  const [contacts, setContacts] = useState(contactList)
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-      <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-        </tr>
-
-        {contacts.map((contact) => {
-          return (
-            <tr key={contact}>
-              <img className="photo" src={contact.pictureUrl}></img>
-              <th>{contact.name}</th>
-              <th>{contact.popularity}</th>
-              {contact.wonOscar ? <th>🏆</th> : <th></th>}
-              {contact.wonEmmy ? <th>🌟</th> : <th></th>}
-            </tr>
-          )
-        })}
-
-      </table>
+      
+      <div>
+        <ContactList />
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,20 @@
+
+function ContactItem({ event, onDelete }) {
+  //iteration 5
+  const handleDelete = () => {
+    onDelete(event)
+  }
+
+    return (
+        <div>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+
+    )
+}
+
+ContactItem.defaultProps = {
+    handleDelete: () => {}
+  }
+
+export default ContactItem
